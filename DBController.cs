a0d15d8d4d11b18124cd93bff46c207bcb3478a3
inak2002 <118966826+inak2002@users.noreply.github.com>
2023-10-03 19:26:27 +0530

    Add files via upload

---
+++ DBController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Firstapp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DBController : ControllerBase
+    {
+        [HttpPost]
+        public string pname()
+        {
+            DataTable dt = new DataTable();
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString= "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;MultipleActiveResultSets=True";
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "pname";
+            cmd.Parameters.Add("@empname", SqlDbType.VarChar, 10).Value = "chacha";
+            cmd.Parameters.Add("@salary", SqlDbType.Int).Value = 70000;
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(dt);
+            con.Close();
+
+            return dt.Rows[0]["empid"].ToString();
+
+        }
+    }
+}
---
+++ FirstController.cs
@@ -0,0 +1,19 @@
+﻿using Firstapp.entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Firstapp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FirstController : ControllerBase
+    {
+        [HttpGet] 
+        public string Myfirstmethod(string Orderitem)
+        {
+            Kitchen kit = new Kitchen();
+            kit.orderitem = Orderitem;
+            return kit.breakfast();
+        }
+    }
+}
